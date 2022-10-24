2e138996bfdb5d13fc136bb444101685b03fbac6
Daniil <kovalevich.danil@gmail.com>
2022-10-24 22:09:09 +0200

    TopPanel was created

---
+++ src/components/TopPanel.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class TopPanel extends Component { 
+    render(){
+        return(
+            <div className="top-panel">
+            </div>
+        )
+    }
+}
+
+export default TopPanel;
