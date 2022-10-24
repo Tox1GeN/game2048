84e5c4ca413f9df37df39fc885858b315cbe62f5
Daniil <kovalevich.danil@gmail.com>
2022-10-24 20:50:22 +0200

    App new component was created

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './styles.css';
+import Home from './Home.js';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <Home />
+            </div>
+        )
+    }
+}
+
+export default App;
