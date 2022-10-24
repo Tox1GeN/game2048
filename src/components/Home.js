037a2357da6c2524734c6071e5bcf240cbd9f4e0
Daniil <kovalevich.danil@gmail.com>
2022-10-24 20:49:47 +0200

    Home new component was created

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div className="tbl-brd">
+                <div className="table">
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
