5a72552e38dca6dd8ca9f442f4f6ab992b918d2d
Daniil <kovalevich.danil@gmail.com>
2022-10-24 22:09:54 +0200

    TopPanel first use

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './styles.css';
 import Home from './Home.js';
+import TopPanel from './TopPanel.js';
 
 class App extends Component {
 
     render() {
         return (
             <div>
+                <TopPanel />
                 <Home />
             </div>
         )
